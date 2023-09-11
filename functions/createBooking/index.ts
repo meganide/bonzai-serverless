6dcf173553700c7e020a7fa7fc52b73a42cabf91
Meganide <renashassan@hotmail.com>
2023-09-11 13:19:22 +0200

    chore: setup database

--- functions/createBooking/index.ts
+++ functions/createBooking/index.ts
@@ -7,24 +7,11 @@ export const handler = async (
   context: APIGatewayProxyResult
 ) => {
   try {
-    const events = await getAllEvents()
-    return sendResponse(200, events)
+    return sendResponse(200, { test: "hej" })
   } catch (error) {
     return sendResponse(500, {
       success: false,
       message: "Something went wrong, could not get any events."
     })
   }
-}
-
-async function getAllEvents() {
-  const { Items } = await db
-    .scan({
-      TableName: "events",
-      ProjectionExpression: "id, arena, artist, #d, price, #t, ticketsLeft",
-      ExpressionAttributeNames: { "#d": "date", "#t": "time" }
-    })
-    .promise()
-
-  return Items
 }
