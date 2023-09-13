077df7cbb0281064ecf56fd63fd394b88763c7f7
NguyenRobin <112572624+NguyenRobin@users.noreply.github.com>
2023-09-13 11:44:01 +0200

    Robin/feat cancel booking (#12)

--- src/functions/deleteBooking/index.ts
+++ src/functions/deleteBooking/index.ts
@@ -1,19 +1,93 @@
+import { db } from "@/services"
 import { sendResponse } from "@/utils"
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
-import { DocumentClient } from "aws-sdk/clients/dynamodb"
 
-import { db } from "../../services/db.ts"
+async function findMyBooking(partitionKey: number | string) {
+  const params = {
+    TableName: "Bonzai",
+    KeyConditionExpression: "PK = :partitionKey",
+    ExpressionAttributeValues: { ":partitionKey": "b#" + partitionKey }
+  }
+
+  const { Items } = await db
+    .query(params, (error, data) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log(data)
+      }
+    })
+    .promise()
+
+  return Items
+}
+
+async function deleteBooking(partitionKey: number | string) {
+  const params = {
+    TableName: "Bonzai",
+    Key: { PK: "b#" + partitionKey, SK: "b#" + partitionKey }
+  }
+
+  await db
+    .delete(params, (error, data) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log(data)
+      }
+    })
+    .promise()
+}
+
+function cancelMyBooking(checkInDate: string): boolean {
+  const twoDaysInMilliseconds = 24 * 60 * 60 * 1000 * 2 // 172800000 MS
+  const bookedDateInMilliseconds = new Date(checkInDate).getTime() // booking CheckInDate in MS
+  const now = new Date().getTime() // When request is sent to database, we get the request date in ms
+  const distance = bookedDateInMilliseconds - now
+
+  if (distance >= twoDaysInMilliseconds) {
+    return true
+  } else {
+    return false
+  }
+}
+interface BookingId extends APIGatewayProxyEvent {
+  bookingId: number | string
+}
 
 export const handler = async (
-  event: APIGatewayProxyEvent,
-  context: APIGatewayProxyResult
-) => {
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   try {
-    return sendResponse(200, { test: "hej" })
-  } catch (error) {
+    const { bookingId } = event.pathParameters as unknown as BookingId
+    const booking = await findMyBooking(bookingId)
+
+    if (!booking || !booking.length) {
+      throw new Error("No booking found")
+    }
+
+    const cancelBooking = cancelMyBooking(booking[0].CheckInDate)
+
+    if (cancelBooking) {
+      await deleteBooking(bookingId)
+
+      return sendResponse(200, {
+        success: true,
+        message: `Booking ${booking[0].PK} has successfully been canceled. `
+      })
+    }
+
+    return sendResponse(404, {
+      success: false,
+      message:
+        "Sorry! Check In is in less than 48 hours. It's not possible to cancel this booking"
+    })
+  } catch (error: any) {
     return sendResponse(500, {
       success: false,
-      message: "Something went wrong, could not get any events."
+      message: error.message
+        ? error.message
+        : "Something went wrong. Booking could not be canceled."
     })
   }
 }
--- src/functions/updateBooking/index.ts
+++ src/functions/updateBooking/index.ts
@@ -1,16 +1,56 @@
+import { db } from "@/services"
 import { sendResponse } from "@/utils"
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import { APIGatewayProxyEvent } from "aws-lambda"
 
-export const handler = async (
-  event: APIGatewayProxyEvent,
-  context: APIGatewayProxyResult
-) => {
+type PrimaryKey = number | string
+interface ValidBodyRequest {
+  NumberGuests?: number
+  Type?: string
+  CheckInDate?: string
+  CheckOut?: string
+}
+async function updateMyBooking(
+  partitionKey: PrimaryKey,
+  sortKey: PrimaryKey,
+  body: ValidBodyRequest
+) {
+  let exp = {
+    UpdateExpression: "SET",
+    ExpressionAttributeNames: {},
+    ExpressionAttributeValues: {}
+  }
+
+  // const params = {
+  //   TableName: "bonzai",
+  //   Key: { PK: partitionKey, SK: sortKey },
+  //   UpdateExpression: `SET `
+  // }
+}
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  // const body = JSON.parse(event.body)
+  // console.log(body)
+  // let exp = {
+  //   UpdateExpression: "SET ",
+  //   ExpressionAttributeNames: {},
+  //   ExpressionAttributeValues: {}
+  // }
+  // // for (const [key, value] of Object.entries(body)) {
+  //   // console.log(key, value)
+  //   // console.log((exp.UpdateExpression += `#${key} = :${key},`))
+  //   // exp.UpdateExpression += `#${key} = :${key},`
+  //   // exp.ExpressionAttributeNames[`#${key}`] = key
+  //   // exp.ExpressionAttributeValues[`:${key}`] = value
+  // }
+  // exp.UpdateExpression = exp.UpdateExpression.slice(0, -1)
+  // console.log(exp)
+  // return exp
   try {
     return sendResponse(200, { test: "hej" })
   } catch (error) {
     return sendResponse(500, {
       success: false,
-      message: "Something went wrong, could not get any events."
+      message: "Something went wrong, could not update booking."
     })
   }
 }
