51324f6c60ab33c0db968dfb723427560fd7f250
NguyenRobin <robin.nnguyen@hotmail.com>
2023-09-15 10:38:24 +0200

    feat: delete-booking

--- src/functions/deleteBooking/index.ts
+++ src/functions/deleteBooking/index.ts
@@ -1,42 +1,10 @@
-import { db } from "@/services"
 import { sendResponse } from "@/utils"
-import {
-  APIGatewayProxyEvent,
-  APIGatewayProxyResult,
-  Context
-} from "aws-lambda"
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import { HttpError } from "http-errors"
 
 import { getBookingById } from "../updateBooking/helpers"
+import { checkDatePolicy, deleteBooking } from "./helpers"
 
-async function deleteBooking(partitionKey: number | string) {
-  const params = {
-    TableName: "Bonzai",
-    Key: { PK: "b#" + partitionKey, SK: "b#" + partitionKey }
-  }
-
-  await db
-    .delete(params, (error, data) => {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log(data)
-      }
-    })
-    .promise()
-}
-
-function cancelMyBooking(checkInDate: string): boolean {
-  const twoDaysInMilliseconds = 24 * 60 * 60 * 1000 * 2 // 172800000 MS
-  const bookedDateInMilliseconds = new Date(checkInDate).getTime() // booking CheckInDate in MS
-  const now = new Date().getTime() // When request is sent to database, we get the request date in ms
-  const distance = bookedDateInMilliseconds - now
-
-  if (distance >= twoDaysInMilliseconds) {
-    return true
-  } else {
-    return false
-  }
-}
 type PathParameters = {
   bookingId: string
 }
@@ -46,34 +14,28 @@ export const handler = async (
 ): Promise<APIGatewayProxyResult> => {
   try {
     const { bookingId } = event.pathParameters as unknown as PathParameters
+    const requestDate = new Date().toISOString().split("T")[0]
     const booking = await getBookingById(bookingId)
 
-    if (!booking || !booking.length) {
-      throw new Error("No booking found")
-    }
+    checkDatePolicy(booking[0].checkInDate, requestDate)
 
-    const cancelBooking = cancelMyBooking(booking[0].CheckInDate)
+    await deleteBooking(booking)
 
-    if (cancelBooking) {
-      await deleteBooking(bookingId)
-
-      return sendResponse(200, {
-        success: true,
-        message: `Booking ${booking[0].PK} has successfully been canceled. `
-      })
-    }
-
-    return sendResponse(404, {
-      success: false,
-      message:
-        "Sorry! Check In is in less than 48 hours. It's not possible to cancel this booking"
+    return sendResponse(200, {
+      success: true,
+      message: "Booking has successfully been deleted."
     })
   } catch (error: any) {
+    console.log(error)
+    if (error instanceof HttpError) {
+      return sendResponse(error.statusCode, {
+        success: false,
+        message: error.message
+      })
+    }
     return sendResponse(500, {
       success: false,
-      message: error.message
-        ? error.message
-        : "Something went wrong. Booking could not be canceled."
+      message: "Something went wrong. Booking could not be canceled."
     })
   }
 }
